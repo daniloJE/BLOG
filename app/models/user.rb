79648b2fc9f02c23501106ddb13419ef6480c521
daniloJE <danidaniposao@gmail.com>
2019-12-20 14:55:57 +0100

    Added password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ApplicationRecord
               uniqueness: {case_sensitive: false},
               format: {with: VALID_EMAIL_REGEX}
     validates :user_id, presence: true
+    has_secure_password
 end
